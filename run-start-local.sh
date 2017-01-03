a7e826651f5394a636640e70d2962707ede628fc
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-01-02 22:48:28 -0500

    Removed deployment scripts in favour of pm2

--- run-start-local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ -z ${API_URL+x} ]; then
-  FINAL_API_URL='"http://localhost:5000"'
-else
-  FINAL_API_URL="\"$API_URL\""
-fi
-echo "Launching with API_URL = $FINAL_API_URL"
-API_URL=$FINAL_API_URL NODE_ENV=development node server.js
--- run-start-prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ -z ${API_URL+x} ]; then
-  echo "API_URL must be defined. Try this format: API_URL='\"http://123.456.78.90:1234\"' npm run start-prod. Remember the quotes and the http prefix!"
-else
-  API_URL="\"$API_URL\"" NODE_ENV=production node server.js
-fi
